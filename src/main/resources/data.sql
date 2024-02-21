03a474c841ce19ab19f0e545df4666e5639f44d7
Artur_Laptop <ArchieBar@yandex.ru>
2024-02-21 16:06:06 +0300

    feat: Добавить .sql скрипты

---
+++ src/main/resources/data.sql
@@ -0,0 +1,138 @@
+-- Вставка данных в таблицу acceptance_status_list, если она еще не существует
+INSERT INTO acceptance_status_list (id_acceptance_status, acceptance_status)
+SELECT 1, 'Запрос на дружбу'
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM acceptance_status_list
+  WHERE id_acceptance_status = 1
+);
+
+INSERT INTO acceptance_status_list (id_acceptance_status, acceptance_status)
+SELECT 2, 'Запрос принят'
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM acceptance_status_list
+  WHERE id_acceptance_status = 2
+);
+
+INSERT INTO acceptance_status_list (id_acceptance_status, acceptance_status)
+SELECT 3, 'Запрос отклонен'
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM acceptance_status_list
+  WHERE id_acceptance_status = 3
+);
+
+-- Вставка данных в таблицу ratings_list, если она еще не существует
+INSERT INTO ratings_list (id_rating, rating)
+SELECT 1, 'G'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM ratings_list
+    WHERE id_rating = 1
+);
+
+INSERT INTO ratings_list (id_rating, rating)
+SELECT 2, 'PG'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM ratings_list
+    WHERE id_rating = 2
+);
+
+INSERT INTO ratings_list (id_rating, rating)
+SELECT 3, 'PG-13'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM ratings_list
+    WHERE id_rating = 3
+);
+
+INSERT INTO ratings_list (id_rating, rating)
+SELECT 4, 'R'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM ratings_list
+    WHERE id_rating = 4
+);
+
+INSERT INTO ratings_list (id_rating, rating)
+SELECT 5, 'NC-17'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM ratings_list
+    WHERE id_rating = 5
+);
+
+-- Вставка данных в таблицу genres_list, если она еще не существует
+INSERT INTO genres_list (id_genre, genre)
+SELECT 1, 'Боевик'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres_list
+    WHERE id_genre = 1
+);
+
+INSERT INTO genres_list (id_genre, genre)
+SELECT 2, 'Комедия'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres_list
+    WHERE id_genre = 2
+);
+
+INSERT INTO genres_list (id_genre, genre)
+SELECT 3, 'Драма'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres_list
+    WHERE id_genre = 3
+);
+
+INSERT INTO genres_list (id_genre, genre)
+SELECT 4, 'Фантастика'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres_list
+    WHERE id_genre = 4
+);
+
+INSERT INTO genres_list (id_genre, genre)
+SELECT 5, 'Ужасы'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres_list
+    WHERE id_genre = 5
+);
+
+INSERT INTO genres_list (id_genre, genre)
+SELECT 6, 'Триллер'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres_list
+    WHERE id_genre = 6
+);
+
+INSERT INTO genres_list (id_genre, genre)
+SELECT 7, 'Приключения'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres_list
+    WHERE id_genre = 7
+);
+
+INSERT INTO genres_list (id_genre, genre)
+SELECT 8, 'Фэнтези'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres_list
+    WHERE id_genre = 8
+);
+
+INSERT INTO genres_list (id_genre, genre)
+SELECT 9, 'Вестерн'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres_list
+    WHERE id_genre = 9
+);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,71 @@
+-- Создание таблицы user, если она еще не существует
+CREATE TABLE IF NOT EXISTS users (
+  id_user INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  login VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255) NOT NULL,
+  birthday DATE NOT NULL,
+  PRIMARY KEY (id_user)
+);
+
+-- Создание таблицы acceptance_status_list, если она еще не существует
+CREATE TABLE IF NOT EXISTS acceptance_status_list (
+  id_acceptance_status INT NOT NULL AUTO_INCREMENT,
+  acceptance_status VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id_acceptance_status)
+);
+
+-- Создание таблицы users_friendship, если она еще не существует
+CREATE TABLE IF NOT EXISTS users_friendship (
+  id_user_1 INT NOT NULL,
+  id_user_2 INT NOT NULL,
+  id_acceptance_status INT NOT NULL,
+  PRIMARY KEY (id_user_1, id_user_2),
+  FOREIGN KEY (id_user_1) REFERENCES users (id_user),
+  FOREIGN KEY (id_user_2) REFERENCES users (id_user),
+  FOREIGN KEY (id_acceptance_status) REFERENCES acceptance_status_list (id_acceptance_status)
+);
+
+-- Создание таблицы ratings_list, если она еще не существует
+CREATE TABLE IF NOT EXISTS ratings_list (
+  id_rating INT NOT NULL AUTO_INCREMENT,
+  rating VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id_rating)
+);
+
+-- Создание таблицы film, если она еще не существует
+CREATE TABLE IF NOT EXISTS film (
+  id_film INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  release_date DATE NOT NULL,
+  duration INT NOT NULL,
+  id_rating INT NOT NULL,
+  PRIMARY KEY (id_film),
+  FOREIGN KEY (id_rating) REFERENCES ratings_list (id_rating)
+);
+
+-- Создание таблицы film_likes, если она еще не существует
+CREATE TABLE IF NOT EXISTS film_likes (
+  id_film INT NOT NULL,
+  id_user INT NOT NULL,
+  PRIMARY KEY (id_film, id_user),
+  FOREIGN KEY (id_film) REFERENCES film (id_film),
+  FOREIGN KEY (id_user) REFERENCES users (id_user)
+);
+
+-- Создание таблицы genres_list, если она еще не существует
+CREATE TABLE IF NOT EXISTS genres_list (
+  id_genre INT NOT NULL AUTO_INCREMENT,
+  genre VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id_genre)
+);
+
+-- Создание таблицы genres_film, если она еще не существует
+CREATE TABLE IF NOT EXISTS genres_film (
+  id_film INT NOT NULL,
+  id_genre INT NOT NULL,
+  PRIMARY KEY (id_film, id_genre),
+  FOREIGN KEY (id_film) REFERENCES film (id_film),
+  FOREIGN KEY (id_genre) REFERENCES genres_list (id_genre)
+);
