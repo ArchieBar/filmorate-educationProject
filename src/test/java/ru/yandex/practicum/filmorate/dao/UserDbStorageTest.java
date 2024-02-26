21da0865518057e4962ab1e38c2fb208b84e4aef
Artur_Laptop <ArchieBar@yandex.ru>
2024-02-27 00:59:24 +0300

    fix: Правки стиля Э й q

--- src/test/java/ru/yandex/practicum/filmorate/dao/UserDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/dao/UserDbStorageTest.java
@@ -9,25 +9,22 @@ import ru.yandex.practicum.filmorate.model.user.User;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@JdbcTest // указываем, о необходимости подготовить бины для работы с БД
+@JdbcTest
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class UserDbStorageTest {
     private final JdbcTemplate jdbcTemplate;
 
     @Test
     public void testFindUserById() {
-        // Подготавливаем данные для теста
         User newUser = new User(1, "user@email.ru", "vanya123", "Ivan Petrov", "1990-01-01");
         UserDbStorage userStorage = new UserDbStorage(jdbcTemplate);
         userStorage.createUser(newUser);
 
-        // вызываем тестируемый метод
         User savedUser = userStorage.findUserById(1);
 
-        // проверяем утверждения
         assertThat(savedUser)
-                .isNotNull() // проверяем, что объект не равен null
-                .usingRecursiveComparison() // проверяем, что значения полей нового
-                .isEqualTo(newUser);        // и сохраненного пользователя - совпадают
+                .isNotNull()
+                .usingRecursiveComparison()
+                .isEqualTo(newUser);
     }
 } 
